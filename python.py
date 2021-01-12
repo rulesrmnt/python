b84a28ba44d4efdf880983dfe9dfdc6988c24392
rulesrmnt <2009coolrahul@gmail.com>
2021-01-12 20:11:02 +0530

    python hello world commit

---
+++ python.py
@@ -0,0 +1,13 @@
+# with double inverted commas
+print ("hello world")
+# with single inverted commas
+print('hello world')
+# with double and single inverted commas
+print ('hello "world" 2')
+print ("helo '2' world")
+# Escape Sequence
+print('line A\n line B\n line C')
+# tab function
+print ('line A \tline B')
+# print double backslash
+print ('this is double \\\\ back slash')
