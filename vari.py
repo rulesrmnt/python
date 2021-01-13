57575d96deb8f5c02282143103fae4b69d0ac3f9
rulesrmnt <2009coolrahul@gmail.com>
2021-01-13 12:03:37 +0530

    varibles declaration

---
+++ vari.py
@@ -0,0 +1,9 @@
+number1=23
+print (number1)
+first_name = "Rahul"
+second_name ="ranjan"
+ag= 24
+full_name = first_name+" "+second_name
+print("My full name is \n"+ full_name+ " \nand age is \n" + str(ag))
+print("My full name is "+ full_name+ " and age is " + str(ag))
+print(f"my full name is {second_name} and age is {ag}")
