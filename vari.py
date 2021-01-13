a6b4f71f048c295df062747cfa64b174cc79512b
rulesrmnt <2009coolrahul@gmail.com>
2021-01-13 12:11:55 +0530

    sum two numbers and name

--- vari.py
+++ vari.py
@@ -7,3 +7,11 @@ full_name = first_name+" "+second_name
 print("My full name is \n"+ full_name+ " \nand age is \n" + str(ag))
 print("My full name is "+ full_name+ " and age is " + str(ag))
 print(f"my full name is {second_name} and age is {ag}")
+
+# user input function
+name =input("enter ur name")
+print ("ur name is " + name)
+number_one=int(input ("enter first number"))
+number_two=int(input ("enter second number"))
+sum=number_one + number_two
+print (str(sum))
