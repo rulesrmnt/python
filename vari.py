dfac8426dc59fd1e8c6bc24fce11d1b0bcaa1ea6
rulesrmnt <2009coolrahul@gmail.com>
2021-01-13 15:32:23 +0530

    spilt case used

--- vari.py
+++ vari.py
@@ -9,15 +9,19 @@ print("My full name is "+ full_name+ " and age is " + str(ag))
 print(f"my full name is {second_name} and age is {ag}")
 
 # user input function
-# name =input("enter ur name")
-# print ("ur name is " + name)
-# number_one=int(input ("enter first number"))
-# number_two=int(input ("enter second number"))
-# sum=number_one + number_two
-# print (str(sum))
+name =input("enter ur name : ")
+print ("ur name is " + name)
+number_one=int(input ("enter first number : "))
+number_two=int(input ("enter second number : "))
+sum=number_one + number_two
+print ("Sum of two numbers is " + str(sum))
 
-num1=int(input('enter 1 no'))
-num2=int(input('enter 2 no'))
-num3=int(input('enter 3 no'))
+num1=int(input('enter 1 no : '))
+num2=int(input('enter 2 no : '))
+num3=int(input('enter 3 no : '))
 Average=(num1 + num2 +num3)//3
-print(str(Average))
+print("Average of three numbers is " + str(Average))
+
+num1, num2, num3=input('enter 3 no : ').split(",")
+Average=(int(num1) + int(num2) +int(num3))//3
+print("Average of three numbers is " + str(Average))
