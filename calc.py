961f38d094e21b5de1da5cf7ad4deb1b8d338469
rulesrmnt <2009coolrahul@gmail.com>
2021-01-13 11:36:22 +0530

    calcualtion in python

---
+++ calc.py
@@ -0,0 +1,9 @@
+print (2+3) #addition
+print (2*3) #multiplication
+print (2-3) # substraction
+print (2/3) #float division
+print (2//3) #integer division
+print (3%2) # Module, gives remainder
+print (2%2)
+print(round(2/3,2)) #round function
+print (2**5) # exponent i.e. 2 to the power 5
