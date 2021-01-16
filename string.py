a2f2f267a4d1883c283a8a7df87e3bb1a80866af
rulesrmnt <2009coolrahul@gmail.com>
2021-01-16 08:45:30 +0530

    length, uppercase, lowercase, title case, count

--- string.py
+++ string.py
@@ -3,11 +3,19 @@ name='rahul'
 print (name[2])
 
 #character slicing
-name2="rahul is good bot"
+name2="raHul is gOOd boT"
 print (name2[0:10])
 print (name2[1:-5])
 print (name2[1:9:2]) #print alternate characters
-print (name2[15::-1])
 print (name2[::-1])
+print (name2[::-1])
+#user input and print in reverse order
 name3=input("enter usename : ")
 print(name3[::-1])
+
+print (len(name2))
+print (name2.upper()) #upper case 
+print (name2.lower()) # lower case
+print (name2.title()) #title case
+print (name2.count('O'))
+print (name2.count('o'))
