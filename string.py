441384c08f30b1e1e295c61ce90d0501d19eecd9
rulesrmnt <2009coolrahul@gmail.com>
2021-01-16 08:36:13 +0530

    slicing, reverse, alternate character

--- string.py
+++ string.py
@@ -1,3 +1,11 @@
 #to print defined location character
 name='rahul'
 print (name[2])
+
+#character slicing
+name2="rahul is good bot"
+print (name2[0:10])
+print (name2[1:-5])
+print (name2[1:9:2]) #print alternate characters
+print (name2[15::-1])
+print (name2[::-1])
