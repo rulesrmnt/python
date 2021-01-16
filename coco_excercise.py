2e0557d9a60ca516f5ba66b3bace3a921e0efe39
rulesrmnt <2009coolrahul@gmail.com>
2021-01-16 13:05:45 +0530

    if else while statement added

---
+++ coco_excercise.py
@@ -0,0 +1,28 @@
+# name, age=input("type your name and age : ").split()
+# if name[0]=="a" or name[0]=="A" and int(age)>10:
+#     print("you can watch coco movie")
+# else:
+#     print('sorry, you cannot watch coco movie')
+
+# name2 = input("enter your name : ")
+# if name2:
+#     print(f'your name is {name2}')
+# else :
+#     print ('invalid command nothing entered')
+
+# #table programe using while loop
+# number=int(input('please enter number : '))
+# total= 0
+# i = 1
+# while i<=10:
+#     total=number*i
+#     print(f"{number}*{i}={total}")
+#     i +=1
+
+num2=int(input("enter the number : "))
+total2 = 0
+i2= 0
+while i2<=num2:
+    total2 +=i2
+    i2+= 1
+print (total2)
